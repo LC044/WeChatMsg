6412cb2224ee55b0d9bb9789df7ec5d6fc10a641
DzhiWang <2429634486@qq.com>
2023-11-27 19:26:31 +0800

    add chat-images to to_html_

--- app/util/path.py
+++ app/util/path.py
@@ -11,7 +11,8 @@ def get_abs_path(path):
     # return os.path.join(os.getcwd(), 'app/data/icons/404.png')
     if path:
         # if os.path.exists(os.path.join())
-        output_path = dat2pic.decode_dat(os.path.join(MePC().wx_dir, path), './data/image')
+        base_path = os.getcwd() + "/data/image"
+        output_path = dat2pic.decode_dat(os.path.join(MePC().wx_dir, path), base_path) #'./data/image')
         return output_path
     else:
         return os.path.join(os.getcwd(), 'app/data/icons/404.png')
